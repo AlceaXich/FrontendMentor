d68603ddcd4675f44bce06e0d92fef875735b980
AlceaXich <shirmoc.t05@gmail.com>
2023-07-27 23:50:13 -0500

    calculando meses, dias

--- age-calculator-app-main/assets/js/script.js
+++ age-calculator-app-main/assets/js/script.js
@@ -4,24 +4,29 @@ let currentDay = currentDate.getDate();
 let currentMonth = currentDate.getMonth() + 1;
 let currentYear = currentDate.getFullYear();
 
+
 function obtenerDatos(){
     let dayElement = document.getElementById("day");
     let monthElement = document.getElementById("month");
     let yearElement = document.getElementById("year");
 
     let yearSpanElement = document.getElementById("resultYears");
+    let monthSpanElement = document.getElementById("resultMonths");
+    let daySpanElement =document.getElementById("resultDays");
 
     let valorday = dayElement.value;
     let valormonth = monthElement.value;
     let valoryear = yearElement.value;
 
     let totalyears = currentYear - valoryear;
+    let totalmonths = currentMonth + (12 - valormonth);
+    let totaldays =currentDay -  valorday;
+
     if (currentMonth < valormonth){
         totalyears = totalyears - 1;
     }
-    yearSpanElement.textContent = totalyears;
 
-    console.log("El valoe de day ingresado es: " + valorday);
-    console.log("El valoe de month ingresado es: " + valormonth);
-    console.log("El valoe de year ingresado es: " + valoryear); 
+    yearSpanElement.textContent = totalyears;
+    monthSpanElement.textContent =totalmonths;
+    daySpanElement.textContent =totaldays;
 }
